a20fe663e7fd2e5c561d3e7daf9d848bed93d858
AbhiSharma096 <Abhisheksharma10a@gmail.com>
2023-02-20 17:45:33 +0530

    Now able to select user and start chat with that one but not mannaging firebase database for now.

--- app/src/main/java/com/example/Adaptar/UserAdaptar.kt
+++ app/src/main/java/com/example/Adaptar/UserAdaptar.kt
@@ -4,14 +4,17 @@ package com.example.Adaptar
 import android.annotation.SuppressLint
 import android.view.View
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
 import com.squareup.picasso.Picasso
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.mesenger.ChatActivity
 import com.example.mesenger.R
 import com.example.mesenger.User
+import com.google.firebase.auth.FirebaseAuth
 import de.hdodenhof.circleimageview.CircleImageView
 
 
@@ -32,6 +35,12 @@ import de.hdodenhof.circleimageview.CircleImageView
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val user = userlist[position]
           holder.username.text=user.username
+          holder.itemView.setOnClickListener {
+                val intent = Intent(context, ChatActivity::class.java)
+                intent.putExtra("name",user.username)
+                intent.putExtra("uid",FirebaseAuth.getInstance().currentUser?.uid)
+                context.startActivity(intent)
+          }
           Picasso.get().load(user.url).into(holder.imageUser)
     }
 
--- app/src/main/java/com/example/mesenger/ContactActivity.kt
+++ app/src/main/java/com/example/mesenger/ContactActivity.kt
@@ -14,6 +14,8 @@ import com.example.Adaptar.UserAdaptar
 //import com.example.mesenger.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
+import com.squareup.picasso.Picasso
+import de.hdodenhof.circleimageview.CircleImageView
 
 class ContactActivity : AppCompatActivity() {
 
@@ -24,10 +26,12 @@ class ContactActivity : AppCompatActivity() {
     private lateinit var mDbRef: DatabaseReference
     private lateinit var userlist: ArrayList<User>
     private lateinit var adaptor: UserAdaptar
+    private lateinit var userProfilePic : CircleImageView
 
 
 
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact)
@@ -44,12 +48,24 @@ class ContactActivity : AppCompatActivity() {
         search = findViewById(R.id.Search)
         newRecyclerview.layoutManager = LinearLayoutManager(this)
         newRecyclerview.adapter = adaptor
+        userProfilePic = findViewById(R.id.UserProfilePic)
 
 
         search.setOnClickListener {
             val intent = Intent(this, NewMessages::class.java)
             startActivity(intent)
         }
+        mDbRef.addListenerForSingleValueEvent(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                var profilepic = snapshot.child("url").value.toString()
+                Picasso.get().load(profilepic).into(userProfilePic)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
 
 
         mDbRef.addValueEventListener(object : ValueEventListener{
@@ -58,6 +74,8 @@ class ContactActivity : AppCompatActivity() {
                 userlist.clear()
                 for (postSnapshot in snapshot.children){
                     val currentuser = postSnapshot.getValue(User::class.java)
+                    if (mAuth.currentUser!!.uid == currentuser!!.uid)
+                        continue
                     userlist.add(currentuser!!)
                 }
                 adaptor.notifyDataSetChanged()
@@ -68,6 +86,17 @@ class ContactActivity : AppCompatActivity() {
             }
 
         })
+
+        //get all user from firebase
+              //add to recyclerview
+        //set user image from firebase
+              //set user name from firebase
+
+
+
+       }
+
+
         //adaptor = UserAdaptar(this,newArraylist)
 
 
@@ -98,7 +127,7 @@ class ContactActivity : AppCompatActivity() {
 
 
 
-    }
+
 
     private fun checkifuserisLogedin() {
         val uri = FirebaseAuth.getInstance().uid
